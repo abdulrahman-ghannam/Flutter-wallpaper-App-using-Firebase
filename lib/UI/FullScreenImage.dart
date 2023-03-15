ed7b1ab64be448b54402056e294388c75ada835b
abdulrahman ghannam <abdo.zarzor2017@gmail.com>
2023-03-14 18:30:06 -0700

    new

--- lib/UI/FullScreenImage.dart
+++ lib/UI/FullScreenImage.dart
@@ -7,50 +7,42 @@ class FullScreenImage extends StatelessWidget {
   final LinearGradient backgroundGradien = LinearGradient(colors: [
     Color(0x1000000),
     Color(0x3000000),
+    // Color(0x4000000),
   ], begin: Alignment.topLeft, end: Alignment.bottomRight);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: new SizedBox.expand(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: backgroundGradien
-          ),
-          child: Stack(
-            children: <Widget>[
-              Align(
-                alignment: Alignment.center,
-                child: Hero(
-                  tag: imagePath, 
-                  child: Image.network(imagePath)
-                ),
-              ),
-              Align(
-                alignment: Alignment.topCenter,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-
-                    AppBar(
-                      elevation: 0.0,
-                      backgroundColor: Colors.transparent,
-                      leading: IconButton(
-                        icon: Icon(Icons.close), 
+        body: new SizedBox.expand(
+      child: Container(
+        decoration: BoxDecoration(gradient: backgroundGradien),
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.center,
+              child: Hero(tag: imagePath, child: Image.network(imagePath)),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  AppBar(
+                    elevation: 0.0,
+                    backgroundColor: Colors.transparent,
+                    leading: IconButton(
+                        icon: Icon(Icons.close),
                         onPressed: () {
                           Navigator.pop(context);
-                        }
-                      ),
-                    )
-
-                  ],
-                ),
-              )
-            ],
-          ),
+                        }),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
-      )
-    );
+      ),
+    ));
   }
 }
